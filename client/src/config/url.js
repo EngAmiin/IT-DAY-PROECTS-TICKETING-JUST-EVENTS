4bef11afec1ae426c05307d57d65205dd483ce95
Abdulrahman-Haji <77126546+ENG-CJ@users.noreply.github.com>
2024-04-05 14:31:42 +0300

    fixed

--- client/src/config/url.js
+++ client/src/config/url.js
@@ -1,2 +1,2 @@
-const DEV_PRODUCTION = "https://portal-kohl-two.vercel.app/portal/";
+export const DEV_PRODUCTION = "https://portal-kohl-two.vercel.app/portal/";
 // export const DEV_PRODUCTION = "http://localhost:3300/portal/";
