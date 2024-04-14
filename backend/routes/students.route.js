c962e9e521ab043e90f1e783120dc5d218e79b2f
amohamud <cj@gmail.com>
2024-04-14 17:03:39 +0300

    changes 001

--- backend/routes/students.route.js
+++ backend/routes/students.route.js
@@ -19,14 +19,11 @@ const {
   fetchEventReport,
 } = require("../controllers/student.control");
 const router = exp.Router();
-
 router.get("/:id_card/:password", authenticate);
 router.get("/projects/:id", readProjectsForUsers);
-router.get("/checkPass/:id",geCurrentPassword);
+router.get("/checkPass/:id", geCurrentPassword);
 router.get("/current/:id", readCurrentUser);
 router.get("/eventRange/:id", fetchStudentRangeByEvent);
-
-router.delete("/remove/:projectId/:studentId", removeProject);
 
 
 router.get("/semesters", readSemesters);
@@ -36,9 +33,13 @@ router.get("/chart/projects/event-report", fetchEventReport);
 router.get("/readTypes", readTypes);
 router.get("/active-event", readActiveEvent);
 router.get("/view-projects", readProjectsForAdmins);
+
+
 router.post("/", registerStudent);
 router.post("/submitProject", setupProject);
 router.post("/updateStatus", updateStatus);
 router.post("/profile", updateProfile);
+
+router.delete("/remove/:projectId/:studentId", removeProject);
 
 module.exports = router;
