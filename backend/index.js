88b9344f9feb72d617bdec107e82c186c05732ba
amohamud <cj@gmail.com>
2024-04-24 08:51:34 +0300

    url changes and moved

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,12 @@ app.use(exp.json())
 app.use(cors())
 app.get("/", function(req, res, next) {
     return res.status(200).json({
-        message : "welcome to the portal"
+        message : "welcome to the portal",
+        description: "This portal is using for IT-DAY projects only",
+        portal : "JUTSA",
+        association_at : "JAMHUURIYA UNIVERSITY",
+        developer : "Hadaf ICT",
+        support : "jutsa@just.edu.so"
     })
 })
 // routes
@@ -27,7 +32,8 @@ app.use("/portal/projectTypes", typeRouter);
 
 app.use("/", function(req, res){
     return res.status(404).json({
-        message : "the url you are requesting is not found"
+        message : "the url you are requesting is not found",
+        description : "you may request unavailable route, or you may mispelled the url. please make sure the url"
     })
 })
 
